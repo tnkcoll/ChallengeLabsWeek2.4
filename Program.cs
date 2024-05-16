d97e865fd743fc381338800c033d96a5db84a9f8
Ted Collins <theodore.collins2023@outlook.com>
2024-05-16 12:00:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ChallengeLabsWeek2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int width = 0;
+            
+            number = GetNumberAndWidth("Please enter a number");
+            width = GetNumberAndWidth("Please enter the width");
+            CreateTriangle(number, width);
+        }
+
+
+        public static int GetNumberAndWidth(string request)
+        {
+
+            Console.WriteLine(request);
+            return Convert.ToInt32(Console.ReadLine());
+
+
+        }
+
+        public static void CreateTriangle(int num1, int num2)
+        {
+            int[] width = new int[num2];
+
+            for (int i = 0; i < width.Length; i++)
+            {
+                width[i] = num1;
+            }
+
+            for (int i = width.Length-1; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(width[j]);
+                }
+                Console.WriteLine();
+            }
+            
+           
+        }
+    }
+}
